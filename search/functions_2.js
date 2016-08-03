1a62d0f8ee078b8fc01748f8e48e31ea92408504
Benjamin Bercovici <bbercovici@Benjamins-MBP.hsd1.co.comcast.net>
2016-08-02 21:30:20 -0600

    updates

--- search/functions_2.js
+++ search/functions_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['populate',['populate',['../class_selected_point_widget.html#afd1322240157289461a8e97b744519c5',1,'SelectedPointWidget']]]
+  ['populate_5fvertex_5ftable',['populate_vertex_table',['../class_selected_point_widget.html#a69cdf53151b25eeded4ffa98be33d4e0',1,'SelectedPointWidget']]]
 ];
--- search/functions_3.js
+++ search/functions_3.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['selectedpointwidget',['SelectedPointWidget',['../class_selected_point_widget.html#a6973c27e249b2956b820cc67d8e01353',1,'SelectedPointWidget']]],
+  ['selectedpointwidget',['SelectedPointWidget',['../class_selected_point_widget.html#ac1f4fc41153203a0983a40052ed9ca6c',1,'SelectedPointWidget']]],
+  ['setcurrentmode',['SetCurrentMode',['../class_interactor_style.html#abc0006b576bca834cb50a429a4eb5118',1,'InteractorStyle']]],
   ['setmainwindow',['SetMainWindow',['../class_interactor_style.html#aa7d59db5ac179da9469081c14e77b6d0',1,'InteractorStyle']]],
   ['setpoints',['SetPoints',['../class_interactor_style.html#aca72fd8ef4db10f2356795ae9cf32d9b',1,'InteractorStyle']]]
 ];
