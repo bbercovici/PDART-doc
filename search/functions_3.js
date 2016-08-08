947406c75e6d360ab84745a452a04b31435a79de
Benjamin Bercovici <bbercovici@engr2-27-11-dhcp.int.colorado.edu>
2016-08-08 10:12:56 -0600

    updates

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['selectedpointwidget',['SelectedPointWidget',['../class_selected_point_widget.html#a2d312134283751b2454e6a968e60204b',1,'SelectedPointWidget']]],
+  ['set_5factors_5fvisibility',['set_actors_visibility',['../class_main_window.html#a83e5e271fb55608c46856562453d8e99',1,'MainWindow']]],
   ['set_5fcurrent_5fmode',['set_current_mode',['../class_interactor_style.html#ad9dcf7e1d3eade05cc9d3a08bf16217a',1,'InteractorStyle']]],
   ['set_5fdata',['set_data',['../class_selected_point_widget.html#a89e04c5192583d221c85335215e69ccc',1,'SelectedPointWidget']]],
   ['set_5fmainwindow',['set_mainwindow',['../class_interactor_style.html#ab0a55222cb9ce93e7f63866eeb3f6a04',1,'InteractorStyle']]],
