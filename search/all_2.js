c1345a85abbe4903c9dab03e72271193e27b1cbd
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2016-12-15 18:09:18 -0700

    finally fixed doxygen

---
+++ search/all_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['find_5fblob_5fcenter',['find_blob_center',['../class_modify_area_widget.html#afcadc0aec39adcfd93ce04f76a5e2c0d',1,'ModifyAreaWidget']]]
+];
---
+++ search/classes_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['computepgmwidget',['ComputePGMWidget',['../class_compute_p_g_m_widget.html',1,'']]]
+];
