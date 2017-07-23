f0622331b39fd46cd56bb3db7032b0609d04fefc
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2017-07-23 10:57:48 -0600

    updates to documentation

---
+++ search/enumvalues_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['moon',['moon',['../namespace_s_b_g_a_t___c_o_r_e_1_1constants.html#ab825e4caf674d145941007a5102fc6aaafb631292b041f7da66d5072f5fae79ac',1,'SBGAT_CORE::constants']]]
+];
---
+++ search/variables_1.js
@@ -0,0 +1,14 @@
+var searchData=
+[
+  ['clear_5fconsole_5faction',['clear_console_action',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a8f14729719115a6b52137267e64b109b',1,'SBGAT_GUI::Mainwindow']]],
+  ['cm',['cm',['../class_s_b_g_a_t___c_o_r_e_1_1_shape_model.html#a4af03118ef54eae26af5d98e576fbc92',1,'SBGAT_CORE::ShapeModel']]],
+  ['compute_5fglobal_5fpgm_5facceleration_5faction',['compute_global_pgm_acceleration_action',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a7235ce76274b49b0de78cdbd81dee0e0',1,'SBGAT_GUI::Mainwindow']]],
+  ['compute_5fgrav_5fslopes_5faction',['compute_grav_slopes_action',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a86f1ccf88905b1aaac82788a1697bdb2',1,'SBGAT_GUI::Mainwindow']]],
+  ['compute_5fpgm_5facceleration_5faction',['compute_pgm_acceleration_action',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a37d60c01e70b88f0681f26c8b2b16fca',1,'SBGAT_GUI::Mainwindow']]],
+  ['consistent_5fglobal_5facceleration',['consistent_global_acceleration',['../class_s_b_g_a_t___g_u_i_1_1_model_data_wrapper.html#a17fa0695ec522afc6bb9a6a27c245894',1,'SBGAT_GUI::ModelDataWrapper']]],
+  ['consistent_5fglobal_5faccelerations',['consistent_global_accelerations',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a73bfee249e0893b0e5d42d924435390b',1,'SBGAT_GUI::Mainwindow']]],
+  ['consistent_5fgrav_5fslope',['consistent_grav_slope',['../class_s_b_g_a_t___g_u_i_1_1_model_data_wrapper.html#abdfc9bf1ba18098b520b2ff51a875d8f',1,'SBGAT_GUI::ModelDataWrapper']]],
+  ['consistent_5fgrav_5fslopes',['consistent_grav_slopes',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#ad7b658f3fd32974da8a249951df7b1f9',1,'SBGAT_GUI::Mainwindow']]],
+  ['consolemenu',['ConsoleMenu',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#af4abebfb4234d638645244d93ac50b49',1,'SBGAT_GUI::Mainwindow']]],
+  ['coordinates',['coordinates',['../class_s_b_g_a_t___c_o_r_e_1_1_vertex.html#a5a35995abc6e70c9c7c991ae15b3df84',1,'SBGAT_CORE::Vertex']]]
+];
