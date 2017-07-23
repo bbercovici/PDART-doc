1d923aebf90614075a32b031412b58bd27a0d10b
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2017-07-22 22:27:56 -0600

    purge

--- search/all_a.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['open_5fcomputepgmwidget_5faction',['open_ComputePGMWidget_action',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a2d38ab92ae69a479e73fc78fe4470907',1,'SBGAT_GUI::Mainwindow']]],
-  ['open_5fsettings_5fwindow',['open_settings_window',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a31a7ad4a5c7c86d2ffef7ac955149528',1,'SBGAT_GUI::Mainwindow']]],
-  ['open_5fsettings_5fwindow_5faction',['open_settings_window_action',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#ae905830f87e78940eb849b965bf666b2',1,'SBGAT_GUI::Mainwindow']]],
-  ['open_5fshapeinfowidget_5faction',['open_ShapeInfoWidget_action',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a05c3e27c7399f8ca475ae36447357bc5',1,'SBGAT_GUI::Mainwindow']]],
-  ['operator_3d',['operator=',['../class_s_b_g_a_t___c_o_r_e_1_1_ref_frame.html#ad75cee4662d3cb4d01865c77fa123ccf',1,'SBGAT_CORE::RefFrame']]]
-];
--- search/variables_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['modify_5fshape_5faction',['modify_shape_action',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a21ddc15542894679502314ed68ec962a',1,'SBGAT_GUI::Mainwindow']]]
-];
