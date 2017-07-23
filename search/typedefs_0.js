567dbb01b08214d9d370e4f690f21d30452d8600
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2017-07-22 23:56:26 -0600

    updates to documentation

---
+++ search/typedefs_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['edge',['edge',['../class_adjacency___list.html#ad2d0ad868f211a253fc7f5fc09f29b65',1,'Adjacency_List']]]
+];
---
+++ search/variables_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['open_5fsettings_5fwindow_5faction',['open_settings_window_action',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#ae905830f87e78940eb849b965bf666b2',1,'SBGAT_GUI::Mainwindow']]]
+];
