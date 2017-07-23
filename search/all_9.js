1e69031614e64c4b37213552a585c5cbe6d9a22f
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2017-07-23 00:08:03 -0600

    updates to documentation

---
+++ search/all_9.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['mainwindow',['Mainwindow',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html',1,'SBGAT_GUI::Mainwindow'],['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a1394a409e4fa81fabf29e6cb856bddc5',1,'SBGAT_GUI::Mainwindow::Mainwindow()']]],
+  ['modeldatawrapper',['ModelDataWrapper',['../class_s_b_g_a_t___g_u_i_1_1_model_data_wrapper.html',1,'SBGAT_GUI']]]
+];
---
+++ search/classes_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['edge',['Edge',['../class_s_b_g_a_t___c_o_r_e_1_1_edge.html',1,'SBGAT_CORE']]]
+];
