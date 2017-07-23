90bc990163dba3f9a5b5d3ccd6755ae1be992452
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2017-07-22 23:56:26 -0600

    purge before updates

--- search/variables_1.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['lateral_5fdockwidget',['lateral_dockwidget',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a41f302aff39e4ed7d49604fbfba16373',1,'SBGAT_GUI::Mainwindow']]],
-  ['load_5fsettings_5fwindow_5faction',['load_settings_window_action',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a9a5e77a0a2fa1cbd3bd49b49e45e3910',1,'SBGAT_GUI::Mainwindow']]],
-  ['load_5fshape_5fmodel_5faction',['load_shape_model_action',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a1a829d267bc7c284045d1668ce7effbc',1,'SBGAT_GUI::Mainwindow']]],
-  ['log_5fconsole',['log_console',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#aaa9a9f71273208566bf2674fb0ba7025',1,'SBGAT_GUI::Mainwindow']]]
-];
--- search/variables_5.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['save_5fconsole_5faction',['save_console_action',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a2ae5313fa876ab3315fa689b86dbcc65',1,'SBGAT_GUI::Mainwindow']]],
-  ['shape_5ftable',['shape_table',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#af3c26686bbb70e2639d19058dd54c661',1,'SBGAT_GUI::Mainwindow']]],
-  ['show_5fgrav_5fslopes_5faction',['show_grav_slopes_action',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a8b25216a78db85e06f8718779405b4c4',1,'SBGAT_GUI::Mainwindow']]],
-  ['show_5flateral_5fdockwidget_5faction',['show_lateral_dockwidget_action',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a95b2ba447cd1f4208ead19f1d0cbf5b3',1,'SBGAT_GUI::Mainwindow']]],
-  ['status_5fbar',['status_bar',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a7193e4cb651a2e62256937e7148f1546',1,'SBGAT_GUI::Mainwindow']]]
-];
