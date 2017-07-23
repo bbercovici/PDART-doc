19ee8853d2de048866bb83cbbf173cc8d19b6228
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2017-07-23 10:51:07 -0600

    updates to documentation

---
+++ search/all_9.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['kw4_5falpha',['kw4_alpha',['../namespace_s_b_g_a_t___c_o_r_e_1_1constants.html#ab825e4caf674d145941007a5102fc6aaa958473cf7662deefebe60149ef79699a',1,'SBGAT_CORE::constants']]]
+];
