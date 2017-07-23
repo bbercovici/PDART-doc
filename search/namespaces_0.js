061456eefe2c91d9e1adcd37ca347b1abf932f46
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2017-07-23 10:50:02 -0600

    updates to documentation

---
+++ search/namespaces_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['constants',['constants',['../namespace_s_b_g_a_t___c_o_r_e_1_1constants.html',1,'SBGAT_CORE']]],
+  ['sbgat_5fcore',['SBGAT_CORE',['../namespace_s_b_g_a_t___c_o_r_e.html',1,'']]],
+  ['sbgat_5fgui',['SBGAT_GUI',['../namespace_s_b_g_a_t___g_u_i.html',1,'']]]
+];
