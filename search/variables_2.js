7710614cfb010d6257c6e3eedf30dfad9714ed2a
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2017-07-23 10:53:29 -0600

    purge before updates

--- search/variables_2.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['data',['data',['../struct_adjacency___list_1_1vertex.html#ab5dd773cf2cea686e1a7c07633c9ae07',1,'Adjacency_List::vertex']]],
-  ['dcm_5ffrom_5fparent',['dcm_from_parent',['../class_s_b_g_a_t___c_o_r_e_1_1_ref_frame.html#a944dff931e455c372a299dd9df30d6da',1,'SBGAT_CORE::RefFrame']]],
-  ['dynamicanalysesmenu',['DynamicAnalysesMenu',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#af4ee3b605e4076e18e88b9e0fc50ac9c',1,'SBGAT_GUI::Mainwindow']]]
-];
