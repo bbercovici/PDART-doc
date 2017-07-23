f0d121bb555a6592e08ffe988bef6eec760a54a9
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2017-07-23 10:19:23 -0600

    updates to documentation

---
+++ search/variables_8.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['mainwindow',['mainwindow',['../class_interactor_style.html#a88fba62a1e05c4ed7f610fc63e2f1e18',1,'InteractorStyle']]],
+  ['mapper',['mapper',['../class_s_b_g_a_t___g_u_i_1_1_model_data_wrapper.html#a4abf0f17b365a4ea4255b7a726749323',1,'SBGAT_GUI::ModelDataWrapper']]],
+  ['mappers',['mappers',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#ac21658166bf1c46b67396a1e38c4ea28',1,'SBGAT_GUI::Mainwindow']]],
+  ['mrp_5ffrom_5fparent',['mrp_from_parent',['../class_s_b_g_a_t___c_o_r_e_1_1_ref_frame.html#a3cff7f395bcc1614f91174793e6b74c6',1,'SBGAT_CORE::RefFrame']]]
+];
