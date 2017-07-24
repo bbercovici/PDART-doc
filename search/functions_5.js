60c2f1880b4091e68bd8ffc2a70b4e3a412559e5
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2017-07-23 20:55:37 -0600

    updates to documentation

---
+++ search/functions_5.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['facet',['Facet',['../class_s_b_g_a_t___c_o_r_e_1_1_facet.html#a520a863f32c2c6ff8c64a6d245178a30',1,'SBGAT_CORE::Facet']]],
+  ['facetresults',['FacetResults',['../class_s_b_g_a_t___c_o_r_e_1_1_facet_results.html#a025f6c2f34c70f2b6cdc8edfb7c936e2',1,'SBGAT_CORE::FacetResults']]],
+  ['finished',['finished',['../class_s_b_g_a_t___g_u_i_1_1_worker.html#a666177ab04bd020fa7d71f0720c27f43',1,'SBGAT_GUI::Worker']]],
+  ['framegraph',['FrameGraph',['../class_s_b_g_a_t___c_o_r_e_1_1_frame_graph.html#a4ae0e79ba49c52a344e924029c081859',1,'SBGAT_CORE::FrameGraph']]],
+  ['free_5fgui',['free_gui',['../class_s_b_g_a_t___g_u_i_1_1_worker.html#a2491c9e5f292e37883982ecf6e6a4736',1,'SBGAT_GUI::Worker']]]
+];
---
+++ search/variables_b.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['parent',['parent',['../class_s_b_g_a_t___g_u_i_1_1_settings_window.html#aebefb8baf172d2ac801b032deeb88df5',1,'SBGAT_GUI::SettingsWindow']]],
+  ['polydata',['polydata',['../class_s_b_g_a_t___g_u_i_1_1_model_data_wrapper.html#a740ef62796cffbda27c8c9484e484fb9',1,'SBGAT_GUI::ModelDataWrapper']]],
+  ['print_5finertia_5faction',['print_inertia_action',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#afaa0e081af08b26267629286f17b4ea4',1,'SBGAT_GUI::Mainwindow']]],
+  ['print_5fsurface_5faction',['print_surface_action',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a88d91018da363c09f4f42aa2003fdfb1',1,'SBGAT_GUI::Mainwindow']]],
+  ['print_5fvolume_5faction',['print_volume_action',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#af22b6f776433f9e4b6c2ccdba8d8f81a',1,'SBGAT_GUI::Mainwindow']]]
+];
