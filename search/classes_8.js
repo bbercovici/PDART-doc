8f03427b6d62dc731b487ec8ad25bea2832c8f70
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2017-07-23 20:56:23 -0600

    purge before updates

--- search/classes_8.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['vertex',['vertex',['../struct_adjacency___list_1_1vertex.html',1,'Adjacency_List&lt; T1, T2 &gt;::vertex'],['../class_s_b_g_a_t___c_o_r_e_1_1_vertex.html',1,'SBGAT_CORE::Vertex']]],
-  ['vtkobjwriter',['vtkOBJWriter',['../classvtk_o_b_j_writer.html',1,'']]]
-];
--- search/functions_8.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['load_5fshape_5fmodel',['load_shape_model',['../class_s_b_g_a_t___c_o_r_e_1_1_shape_model_importer.html#a1422f5baf7fc6d010e1e0ed5a3a207b4',1,'SBGAT_CORE::ShapeModelImporter::load_shape_model()'],['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a5258a91ffde589ce6611e80c6d3fd2c8',1,'SBGAT_GUI::Mainwindow::load_shape_model()']]],
-  ['logging_5fout',['logging_out',['../class_s_b_g_a_t___g_u_i_1_1_worker.html#a0bdccc139e38989dbef66c64ac4d2da5',1,'SBGAT_GUI::Worker']]]
-];
