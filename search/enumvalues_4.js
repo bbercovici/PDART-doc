357dec21f87b327af5cc9a3b86187fda7cf299c1
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2017-07-23 20:55:36 -0600

    purge before updates

--- search/enumvalues_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['moon',['moon',['../namespace_s_b_g_a_t___c_o_r_e_1_1constants.html#ab825e4caf674d145941007a5102fc6aaafb631292b041f7da66d5072f5fae79ac',1,'SBGAT_CORE::constants']]]
-];
