04111a3a1d2fc9d130da699de2217c1b212f8eb6
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2017-08-02 18:48:57 -0600

    updates to documentation

---
+++ search/all_14.js
@@ -0,0 +1,21 @@
+var searchData=
+[
+  ['v0',['v0',['../class_s_b_g_a_t___c_o_r_e_1_1_edge.html#a22e009a015268152e882738cacb988bc',1,'SBGAT_CORE::Edge']]],
+  ['v1',['v1',['../class_s_b_g_a_t___c_o_r_e_1_1_edge.html#adca18665d66f724406a2d23d1e187557',1,'SBGAT_CORE::Edge']]],
+  ['vertex',['vertex',['../struct_adjacency___list_1_1vertex.html',1,'Adjacency_List&lt; T1, T2 &gt;::vertex'],['../class_s_b_g_a_t___c_o_r_e_1_1_vertex.html',1,'SBGAT_CORE::Vertex'],['../struct_adjacency___list_1_1vertex.html#ace630ecbe09f25c4f8db743fe67374c8',1,'Adjacency_List::vertex::vertex()']]],
+  ['vertex_2ecpp',['Vertex.cpp',['../_vertex_8cpp.html',1,'']]],
+  ['vertex_2ehpp',['Vertex.hpp',['../_vertex_8hpp.html',1,'']]],
+  ['vertex_5fnot_5fon_5fedge',['vertex_not_on_edge',['../class_s_b_g_a_t___c_o_r_e_1_1_facet.html#af83ba1c42f6fc5bdb43cfa8d88ffa134',1,'SBGAT_CORE::Facet']]],
+  ['vertex_5ftype',['vertex_type',['../class_adjacency___list.html#a36f797555285f40b0320eb745774e127',1,'Adjacency_List']]],
+  ['vertexexists',['vertexexists',['../class_adjacency___list.html#a5fef72d4ecb4cfa5438a653e1e58e91b',1,'Adjacency_List']]],
+  ['vertexmap',['vertexmap',['../class_adjacency___list.html#a5f1ff6746e887afb9fd3eb33f0692e84',1,'Adjacency_List']]],
+  ['vertices',['vertices',['../class_s_b_g_a_t___c_o_r_e_1_1_facet.html#aaccad0e3cd3869a7bd3153356e37fea1',1,'SBGAT_CORE::Facet::vertices()'],['../class_s_b_g_a_t___c_o_r_e_1_1_shape_model.html#a97ed6acd183e86bbabd069e84f27485a',1,'SBGAT_CORE::ShapeModel::vertices()']]],
+  ['viewmenu',['ViewMenu',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#aa4f0360f4d9047ca3e074ceffada5224',1,'SBGAT_GUI::Mainwindow']]],
+  ['vmap',['vmap',['../class_adjacency___list.html#ab4154ab5baaf2c35eb12fdf4d2cc3887',1,'Adjacency_List']]],
+  ['volume',['volume',['../class_s_b_g_a_t___c_o_r_e_1_1_shape_model.html#a4b323af456fb6b51feddc3518266a645',1,'SBGAT_CORE::ShapeModel']]],
+  ['vtkgetstringmacro',['vtkGetStringMacro',['../classvtk_o_b_j_writer.html#a53fa84d4b8f4aff0f77d20bbd1099eee',1,'vtkOBJWriter']]],
+  ['vtkobjwriter',['vtkOBJWriter',['../classvtk_o_b_j_writer.html',1,'vtkOBJWriter'],['../classvtk_o_b_j_writer.html#a8dea8cd4043b1938b01c95dcc999510a',1,'vtkOBJWriter::vtkOBJWriter()'],['../classvtk_o_b_j_writer.html#a711959443345e2b6ab081a920ce8cb00',1,'vtkOBJWriter::vtkOBJWriter(const vtkOBJWriter &amp;)']]],
+  ['vtkobjwriter_2eh',['vtkOBJWriter.h',['../vtk_o_b_j_writer_8h.html',1,'']]],
+  ['vtksetstringmacro',['vtkSetStringMacro',['../classvtk_o_b_j_writer.html#a5f8fda9a9c917d7a2080e41cf6793d9a',1,'vtkOBJWriter']]],
+  ['vtktypemacro',['vtkTypeMacro',['../class_interactor_style.html#a43338f1434b768677a2feac0b1a05c13',1,'InteractorStyle::vtkTypeMacro()'],['../classvtk_o_b_j_writer.html#a8f74032378c16d26e5c7b5936bdd5d42',1,'vtkOBJWriter::vtkTypeMacro()']]]
+];
---
+++ search/variables_10.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['use_5fedges',['use_edges',['../class_s_b_g_a_t___c_o_r_e_1_1_shape_model_importer.html#a18053b659dfba1d8cec4c9d162b548d7',1,'SBGAT_CORE::ShapeModelImporter']]],
+  ['use_5fgradient_5fcheckbox',['use_gradient_checkbox',['../class_s_b_g_a_t___g_u_i_1_1_settings_window.html#a9555557fb8785c2f916f67d2a7d0609e',1,'SBGAT_GUI::SettingsWindow']]]
+];
