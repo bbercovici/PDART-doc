be51a8f43886d158c8440c6c207d77c507f348a3
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2017-08-02 18:52:42 -0600

    updates to documentation

---
+++ search/all_4.js
@@ -0,0 +1,14 @@
+var searchData=
+[
+  ['data',['data',['../struct_adjacency___list_1_1vertex.html#ab5dd773cf2cea686e1a7c07633c9ae07',1,'Adjacency_List::vertex']]],
+  ['dcm_5ffrom_5fparent',['dcm_from_parent',['../class_s_b_g_a_t___c_o_r_e_1_1_ref_frame.html#a944dff931e455c372a299dd9df30d6da',1,'SBGAT_CORE::RefFrame']]],
+  ['ddfs',['ddfs',['../class_adjacency___list.html#a36c640eaabccc946280cac85aa6f5998',1,'Adjacency_List']]],
+  ['density',['density',['../class_s_b_g_a_t___g_u_i_1_1_worker.html#a8ededfba805d8147aeffaef3412c509a',1,'SBGAT_GUI::Worker::density()'],['../namespace_s_b_g_a_t___c_o_r_e_1_1constants.html#ab825e4caf674d145941007a5102fc6aa',1,'SBGAT_CORE::constants::density()']]],
+  ['dfs',['dfs',['../class_adjacency___list.html#a18dd59802c4a04906aec8cc0703254e8',1,'Adjacency_List']]],
+  ['displaygraph',['displaygraph',['../class_adjacency___list.html#a8bd4579e85308ada2431895a71c5f7c7',1,'Adjacency_List']]],
+  ['dyn_5fanalyses',['dyn_analyses',['../class_s_b_g_a_t___g_u_i_1_1_worker.html#a480c3c8db82a16726b1785547775285a',1,'SBGAT_GUI::Worker']]],
+  ['dynamicanalyses',['DynamicAnalyses',['../class_s_b_g_a_t___c_o_r_e_1_1_dynamic_analyses.html',1,'SBGAT_CORE::DynamicAnalyses'],['../class_s_b_g_a_t___c_o_r_e_1_1_dynamic_analyses.html#a031a9e826475dc9c646dd53df46422ed',1,'SBGAT_CORE::DynamicAnalyses::DynamicAnalyses()']]],
+  ['dynamicanalyses_2ecpp',['DynamicAnalyses.cpp',['../_dynamic_analyses_8cpp.html',1,'']]],
+  ['dynamicanalyses_2ehpp',['DynamicAnalyses.hpp',['../_dynamic_analyses_8hpp.html',1,'']]],
+  ['dynamicanalysesmenu',['DynamicAnalysesMenu',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#af4ee3b605e4076e18e88b9e0fc50ac9c',1,'SBGAT_GUI::Mainwindow']]]
+];
