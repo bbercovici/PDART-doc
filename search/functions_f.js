5f17aeba7dde574c3563edc6c684b00d74ce08f2
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2017-08-02 18:52:41 -0600

    purge before updates

--- search/functions_f.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['toggle_5fshape_5fvisibility',['toggle_shape_visibility',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#ab08e3db891f6f20d192dc2334294aaab',1,'SBGAT_GUI::Mainwindow']]]
-];
