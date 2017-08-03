b121da30c52ebc8e5534b1f2143412486ac192d4
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2017-08-02 19:13:22 -0600

    purge before updates

--- search/enumvalues_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['itokawa',['itokawa',['../namespace_s_b_g_a_t___c_o_r_e_1_1constants.html#ab825e4caf674d145941007a5102fc6aaa03562cb4af262e130a978b8692c2995a',1,'SBGAT_CORE::constants']]]
-];
