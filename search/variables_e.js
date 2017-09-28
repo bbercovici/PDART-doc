a774a5a703ec5c620f3775d600a5246278583a20
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2017-09-28 17:00:23 -0600

    purge before updates

--- search/variables_e.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['ref_5fframe_5fname',['ref_frame_name',['../class_s_b_g_a_t___c_o_r_e_1_1_shape_model.html#ad6cad59b1260dff560a207619b23d2a3',1,'SBGAT_CORE::ShapeModel']]],
-  ['ref_5fnames_5fto_5fref_5fptrs',['ref_names_to_ref_ptrs',['../class_s_b_g_a_t___c_o_r_e_1_1_frame_graph.html#a57c065426e558c9f52edc76c1083bf82',1,'SBGAT_CORE::FrameGraph']]],
-  ['renderer',['renderer',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#ab40630e7d9734765032b79aa68f39e37',1,'SBGAT_GUI::Mainwindow']]],
-  ['resultsmenu',['ResultsMenu',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#ac6d9b2de027e76f7612c1241f96119a7',1,'SBGAT_GUI::Mainwindow']]],
-  ['rgb_5fbutton',['rgb_button',['../class_s_b_g_a_t___g_u_i_1_1_settings_window.html#a62c7a9c6df790af92a443134699c1ac8',1,'SBGAT_GUI::SettingsWindow']]]
-];
