0cb0deaae9e124a8a3d34ba74018f5b0b5cdbc29
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2017-10-05 11:04:50 -0600

    purge before updates

--- search/files_3.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['edge_2ecpp',['Edge.cpp',['../_edge_8cpp.html',1,'']]],
-  ['edge_2ehpp',['Edge.hpp',['../_edge_8hpp.html',1,'']]]
-];
--- search/functions_12.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['worker',['Worker',['../class_s_b_g_a_t___g_u_i_1_1_worker.html#a59747db266d8eb06b089b09d7e6d70ff',1,'SBGAT_GUI::Worker']]]
-];
