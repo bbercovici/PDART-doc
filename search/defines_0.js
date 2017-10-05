6bd65c7b58c8de82880ccbb23bced7679a6d4aa7
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2017-10-05 11:10:42 -0600

    updates to documentation

---
+++ search/defines_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['dimmax',['dimMax',['../_polyhedral_spherical_harmo_8hpp.html#acaf0e6c41a69eadb78fec587f3841d6e',1,'PolyhedralSphericalHarmo.hpp']]]
+];
---
+++ search/functions_4.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['edge',['Edge',['../class_s_b_g_a_t___c_o_r_e_1_1_edge.html#a8abe18ac45d5909c9d94bc758e55412a',1,'SBGAT_CORE::Edge']]],
+  ['edgeexist',['edgeexist',['../class_adjacency___list.html#a4940d2076901a50659ca83894b945218',1,'Adjacency_List']]],
+  ['enforce_5fmesh_5fquality',['enforce_mesh_quality',['../class_s_b_g_a_t___c_o_r_e_1_1_shape_model.html#a9caf81e9f4d48ddc4bf6bd3f45147747',1,'SBGAT_CORE::ShapeModel']]],
+  ['error',['error',['../class_s_b_g_a_t___g_u_i_1_1_worker.html#ab0058c3dfaea849eac0dc3aed021e794',1,'SBGAT_GUI::Worker']]]
+];
