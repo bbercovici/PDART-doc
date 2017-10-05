fe876085797684e8af78c4e33dbd76b7587c05c3
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2017-10-05 10:53:34 -0600

    purge before updates

--- search/enums_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['density',['density',['../namespace_s_b_g_a_t___c_o_r_e_1_1constants.html#ab825e4caf674d145941007a5102fc6aa',1,'SBGAT_CORE::constants']]]
-];
--- search/variables_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['barycenter_5faligned',['barycenter_aligned',['../class_s_b_g_a_t___c_o_r_e_1_1_shape_model.html#afd2332f8f576ecefe8bd7306ee8cc8ed',1,'SBGAT_CORE::ShapeModel']]]
-];
