a4820e3e05e59646d7a1604ff9bac8fade02859c
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-10-12 15:28:12 -0600

    updates to documentation

---
+++ search/classes_5.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['settingswindow',['SettingsWindow',['../class_s_b_g_a_t___g_u_i_1_1_settings_window.html',1,'SBGAT_GUI']]],
+  ['shapepropertieswidget',['ShapePropertiesWidget',['../class_s_b_g_a_t___g_u_i_1_1_shape_properties_widget.html',1,'SBGAT_GUI']]],
+  ['sharmwindow',['SHARMWindow',['../class_s_b_g_a_t___g_u_i_1_1_s_h_a_r_m_window.html',1,'SBGAT_GUI']]]
+];
