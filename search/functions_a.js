94145cde191c9e180b928c1a87acff9a2f5d3046
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-10-12 15:04:29 -0600

    purge before updates

--- search/functions_a.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['radarvisualizer',['RadarVisualizer',['../class_s_b_g_a_t___g_u_i_1_1_radar_visualizer.html#a729719c29f7270fcd6b0a65350938978',1,'SBGAT_GUI::RadarVisualizer']]],
-  ['radarwindow',['RadarWindow',['../class_s_b_g_a_t___g_u_i_1_1_radar_window.html#a3807c6a6bef21a81fa596820d4db8e83',1,'SBGAT_GUI::RadarWindow']]],
-  ['remove_5fprop',['remove_prop',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a9fdad16619cadbd5112d0b000e427408',1,'SBGAT_GUI::Mainwindow']]],
-  ['renderingpropertieswindow',['RenderingPropertiesWindow',['../class_s_b_g_a_t___g_u_i_1_1_rendering_properties_window.html#a5a9fe26fb3e46785bc46b9e4cb3a2173',1,'SBGAT_GUI::RenderingPropertiesWindow']]]
-];
