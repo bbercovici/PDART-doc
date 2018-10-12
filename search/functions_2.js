455f1f54c6a6b130de2372803a8a514673b05715
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-10-12 15:42:35 -0600

    updates to documentation

---
+++ search/functions_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['bin_5fobservations',['bin_observations',['../class_s_b_g_a_t___g_u_i_1_1_radar_window.html#a0e993e1a2e99dea5538ff49ac606efb9',1,'SBGAT_GUI::RadarWindow']]],
+  ['binobservations',['BinObservations',['../class_s_b_g_a_t_obs_radar.html#a459b4a782aa1907d19473c8a457c34e4',1,'SBGATObsRadar']]]
+];
