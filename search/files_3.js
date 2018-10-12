4eb64098c21c3c3807a345ca5e9978ff757b44f7
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-10-12 15:22:56 -0600

    purge before updates

--- search/files_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['obswindow_2ehpp',['ObsWindow.hpp',['../_obs_window_8hpp.html',1,'']]]
-];
--- search/variables_6.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['images',['images',['../class_s_b_g_a_t___g_u_i_1_1_radar_visualizer.html#ae39ea9441722569e938d2050f26d7bae',1,'SBGAT_GUI::RadarVisualizer']]],
-  ['imaging_5fperiod_5fsbox',['imaging_period_sbox',['../class_s_b_g_a_t___g_u_i_1_1_obs_window.html#abff306be75a9f082d3db6d80bc33b94f',1,'SBGAT_GUI::ObsWindow']]],
-  ['inc_5fsbox',['inc_sbox',['../class_s_b_g_a_t___g_u_i_1_1_shape_properties_widget.html#ad02a3acb33d5c2529a57d3601f215642',1,'SBGAT_GUI::ShapePropertiesWidget']]],
-  ['is_5fprimary',['is_primary',['../class_s_b_g_a_t___g_u_i_1_1_shape_properties_widget.html#a18b568f4e6e567c00bae87a96f227048',1,'SBGAT_GUI::ShapePropertiesWidget']]]
-];
