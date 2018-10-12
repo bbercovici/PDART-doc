e4140f8090587580a97ceb6b8af87aff2fc503ef
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-10-12 15:28:07 -0600

    purge before updates

--- search/variables_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['ecc_5fsbox',['ecc_sbox',['../class_s_b_g_a_t___g_u_i_1_1_shape_properties_widget.html#aab9f9f60dcf8407770447ccc8f1fa6f3',1,'SBGAT_GUI::ShapePropertiesWidget']]]
-];
