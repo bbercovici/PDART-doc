4cd4dcb32e2adf0be7cb4c42fb8087fefa739f7a
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-10-12 15:46:48 -0600

    updates to documentation

---
+++ search/variables_2.js
@@ -0,0 +1,15 @@
+var searchData=
+[
+  ['center_5fof_5fmass',['center_of_mass',['../class_s_b_g_a_t_mass_properties.html#a8516e5cf1a054b01b0207cfe4cce2768',1,'SBGATMassProperties']]],
+  ['center_5fof_5fmass_5fvec',['center_of_mass_vec',['../class_s_b_g_a_t_obs.html#a38b49c79c9a6c213f8916a95a7e8eb79',1,'SBGATObs']]],
+  ['clear_5fconsole_5faction',['clear_console_action',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a8f14729719115a6b52137267e64b109b',1,'SBGAT_GUI::Mainwindow']]],
+  ['cnm',['Cnm',['../class_s_b_g_a_t_spherical_harmo.html#a453d32f4e87259125f291bc0e05f2dcd',1,'SBGATSphericalHarmo']]],
+  ['collect_5fobservations_5fbutton',['collect_observations_button',['../class_s_b_g_a_t___g_u_i_1_1_obs_window.html#a1c2d1d68c47c45b08fd71568c2e53979',1,'SBGAT_GUI::ObsWindow']]],
+  ['compute_5fgeometric_5fmeasures_5faction',['compute_geometric_measures_action',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a28782658fdc20730b1be4f862682210b',1,'SBGAT_GUI::Mainwindow']]],
+  ['consistent_5fglobal_5facceleration',['consistent_global_acceleration',['../class_s_b_g_a_t___g_u_i_1_1_model_data_wrapper.html#a17fa0695ec522afc6bb9a6a27c245894',1,'SBGAT_GUI::ModelDataWrapper']]],
+  ['consistent_5fglobal_5fpotential',['consistent_global_potential',['../class_s_b_g_a_t___g_u_i_1_1_model_data_wrapper.html#a24e08e7f20b017325d735af8e124ac9d',1,'SBGAT_GUI::ModelDataWrapper']]],
+  ['consistent_5fgrav_5fslope',['consistent_grav_slope',['../class_s_b_g_a_t___g_u_i_1_1_model_data_wrapper.html#abdfc9bf1ba18098b520b2ff51a875d8f',1,'SBGAT_GUI::ModelDataWrapper']]],
+  ['consistent_5fshape_5fmodel',['consistent_shape_model',['../class_s_b_g_a_t___g_u_i_1_1_model_data_wrapper.html#a37c93113c19527065d702d1c523466ce',1,'SBGAT_GUI::ModelDataWrapper']]],
+  ['consolemenu',['ConsoleMenu',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#af4abebfb4234d638645244d93ac50b49',1,'SBGAT_GUI::Mainwindow']]],
+  ['current_5fimage_5findex',['current_image_index',['../class_s_b_g_a_t___g_u_i_1_1_radar_visualizer.html#a67860819569bcb6082b3376e8c97211c',1,'SBGAT_GUI::RadarVisualizer']]]
+];
