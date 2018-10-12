8cc91beafe350185d7c4b9cdf7c68815d9efd5e0
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-10-12 15:03:00 -0600

    purge before updates

--- search/functions_5.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['lcvisualizer',['LCVisualizer',['../class_s_b_g_a_t___g_u_i_1_1_l_c_visualizer.html#a20cdf8edaf218c15e8a97b5b37797762',1,'SBGAT_GUI::LCVisualizer']]],
-  ['lcwindow',['LCWindow',['../class_s_b_g_a_t___g_u_i_1_1_l_c_window.html#a7e8a51f5022176fc34b44cca01d34313',1,'SBGAT_GUI::LCWindow']]]
-];
