73132ad32a3999e5661a2f5fa751b7578baa9b59
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-10-12 15:46:03 -0600

    updates to documentation

---
+++ search/all_14.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['update_5factions_5favailability',['update_actions_availability',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#ab238a1ca2fdb88b7ac180f071d99a233',1,'SBGAT_GUI::Mainwindow']]],
+  ['update_5fgui_5fchanged_5fprop',['update_GUI_changed_prop',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#acfb08572289e7df03aadd5e67018f226',1,'SBGAT_GUI::Mainwindow']]],
+  ['update_5fphase_5fangle',['update_phase_angle',['../class_s_b_g_a_t___g_u_i_1_1_l_c_window.html#a06c74819b86b133ff8d3457a0b11ba57',1,'SBGAT_GUI::LCWindow']]],
+  ['use_5fgradient_5fcheckbox',['use_gradient_checkbox',['../class_s_b_g_a_t___g_u_i_1_1_settings_window.html#a9555557fb8785c2f916f67d2a7d0609e',1,'SBGAT_GUI::SettingsWindow']]],
+  ['use_5fomp_5fdynamic_5fanalysis',['USE_OMP_DYNAMIC_ANALYSIS',['../_o_m_p__flags_8hpp.html#a44053aa45ca18739271d96180cd10a3a',1,'OMP_flags.hpp']]],
+  ['use_5fomp_5fshape_5fmodel',['USE_OMP_SHAPE_MODEL',['../_o_m_p__flags_8hpp.html#a68f7e60616abbd8b059c7e72e640505d',1,'OMP_flags.hpp']]]
+];
