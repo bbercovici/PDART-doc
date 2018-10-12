9a888bfc72901c0d624774313aeea29cf3966371
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-10-12 15:43:58 -0600

    updates to documentation

---
+++ search/variables_7.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['images',['images',['../class_s_b_g_a_t_obs_radar.html#a3b6d5d29b875b925f70ea8819c588ea1',1,'SBGATObsRadar::images()'],['../class_s_b_g_a_t___g_u_i_1_1_radar_visualizer.html#ae39ea9441722569e938d2050f26d7bae',1,'SBGAT_GUI::RadarVisualizer::images()']]],
+  ['imaging_5fperiod_5fsbox',['imaging_period_sbox',['../class_s_b_g_a_t___g_u_i_1_1_obs_window.html#abff306be75a9f082d3db6d80bc33b94f',1,'SBGAT_GUI::ObsWindow']]],
+  ['inc_5fsbox',['inc_sbox',['../class_s_b_g_a_t___g_u_i_1_1_shape_properties_widget.html#ad02a3acb33d5c2529a57d3601f215642',1,'SBGAT_GUI::ShapePropertiesWidget']]],
+  ['indentified',['indentified',['../struct_adjacency___list_1_1vertex.html#a2517924d73f06e7b133a8903d43cc1a6',1,'Adjacency_List::vertex']]],
+  ['inertia_5ftensor',['inertia_tensor',['../class_s_b_g_a_t_mass_properties.html#a6cb5282831569eb76b764ab5d71293e3',1,'SBGATMassProperties']]],
+  ['is_5fprimary',['is_primary',['../class_s_b_g_a_t___g_u_i_1_1_shape_properties_widget.html#a18b568f4e6e567c00bae87a96f227048',1,'SBGAT_GUI::ShapePropertiesWidget']]],
+  ['isclosed',['IsClosed',['../class_s_b_g_a_t_mass_properties.html#af7e423d4c72ebb6c3048a3404e856dd9',1,'SBGATMassProperties']]],
+  ['isdirected',['isdirected',['../class_adjacency___list.html#a92ceab755c148a8f0bdb6f8c3ccfcc7f',1,'Adjacency_List']]]
+];
