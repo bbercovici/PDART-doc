e9467abd4da2f03da1c7dd793b604a02e8f7252a
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-10-12 15:43:49 -0600

    purge before updates

--- search/files_4.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['mainwindow_2ehpp',['Mainwindow.hpp',['../_mainwindow_8hpp.html',1,'']]],
-  ['modeldatawrapper_2ehpp',['ModelDataWrapper.hpp',['../_model_data_wrapper_8hpp.html',1,'']]]
-];
