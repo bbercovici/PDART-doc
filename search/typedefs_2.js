16f1685ef7d98de3823e860d40c82ca656c8302a
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-10-12 15:33:19 -0600

    purge before updates

--- search/typedefs_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['sbgatradarobssequence',['SBGATRadarObsSequence',['../_s_b_g_a_t_obs_radar_8hpp.html#ab5371cee1f8394f7ab581e74c5fddf19',1,'SBGATObsRadar.hpp']]]
-];
--- search/variables_11.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['totalmass',['totalMass',['../class_s_b_g_a_t_spherical_harmo.html#a90cd173047adc19a6e2018f04eb83be1',1,'SBGATSphericalHarmo']]]
-];
