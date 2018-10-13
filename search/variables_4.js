7ef765ee2d18a00139a31edcc0e4e5af10f2eadd
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-10-12 18:41:07 -0600

    updates to documentation

---
+++ search/variables_4.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['ecc_5fsbox',['ecc_sbox',['../class_s_b_g_a_t___g_u_i_1_1_shape_properties_widget.html#aab9f9f60dcf8407770447ccc8f1fa6f3',1,'SBGAT_GUI::ShapePropertiesWidget']]],
+  ['edge_5fdyads',['edge_dyads',['../class_s_b_g_a_t_polyhedron_gravity_model.html#ad2b7a8e7b6575d685a775c4f5cc43641',1,'SBGATPolyhedronGravityModel']]],
+  ['edges',['edges',['../class_s_b_g_a_t_polyhedron_gravity_model.html#a00c9fb8bf682d44ca70aebdb4997de79',1,'SBGATPolyhedronGravityModel']]]
+];
