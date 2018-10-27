079898202003236709e26e603cc2fcc6804b88df
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-10-27 16:20:11 -0500

    purge before updates

--- search/classes_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['lcvisualizer',['LCVisualizer',['../class_s_b_g_a_t___g_u_i_1_1_l_c_visualizer.html',1,'SBGAT_GUI']]],
-  ['lcwindow',['LCWindow',['../class_s_b_g_a_t___g_u_i_1_1_l_c_window.html',1,'SBGAT_GUI']]]
-];
--- search/functions_11.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['update_5factions_5favailability',['update_actions_availability',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#ab238a1ca2fdb88b7ac180f071d99a233',1,'SBGAT_GUI::Mainwindow']]],
-  ['update_5fgui_5fchanged_5fprop',['update_GUI_changed_prop',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#acfb08572289e7df03aadd5e67018f226',1,'SBGAT_GUI::Mainwindow']]],
-  ['update_5fphase_5fangle',['update_phase_angle',['../class_s_b_g_a_t___g_u_i_1_1_l_c_window.html#a06c74819b86b133ff8d3457a0b11ba57',1,'SBGAT_GUI::LCWindow']]]
-];
