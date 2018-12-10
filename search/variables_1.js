3ae5b3dbab4cfb2d638396d1f2740b65e91bfb32
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-12-10 09:21:02 -0700

    updates to documentation

---
+++ search/variables_1.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['background_5fcolor_5fgroup',['background_color_group',['../class_s_b_g_a_t___g_u_i_1_1_settings_window.html#a535bfd10b51c7828640bf993fb6cda75',1,'SBGAT_GUI::SettingsWindow']]],
+  ['background_5ftype_5fcombo_5fbox',['background_type_combo_box',['../class_s_b_g_a_t___g_u_i_1_1_settings_window.html#af141a8456546035edac07d8e9ecee017',1,'SBGAT_GUI::SettingsWindow']]],
+  ['bin_5fobservations_5fbutton',['bin_observations_button',['../class_s_b_g_a_t___g_u_i_1_1_radar_window.html#ab807b3dc5f4971310298a337ff887df8',1,'SBGAT_GUI::RadarWindow']]],
+  ['bounces_5fcombo_5fbox',['bounces_combo_box',['../class_s_b_g_a_t___g_u_i_1_1_y_o_r_p_window.html#ac90bed74821c5d5a7fb867ca6b100ade',1,'SBGAT_GUI::YORPWindow']]],
+  ['bounds',['bounds',['../class_s_b_g_a_t_mass_properties.html#a58ff5409f5f590478dd9a99e2944768d',1,'SBGATMassProperties']]],
+  ['bsptree_5fvec',['bspTree_vec',['../class_s_b_g_a_t_obs.html#a6df881a0355dc03d049e924ab9359186',1,'SBGATObs']]],
+  ['button_5fbox',['button_box',['../class_s_b_g_a_t___g_u_i_1_1_l_c_visualizer.html#abcbb79b2b42ef49e84ca073e6318b3e1',1,'SBGAT_GUI::LCVisualizer::button_box()'],['../class_s_b_g_a_t___g_u_i_1_1_obs_window.html#a51707bf162af1bb3f15bbdd86b1b92c5',1,'SBGAT_GUI::ObsWindow::button_box()'],['../class_s_b_g_a_t___g_u_i_1_1_radar_visualizer.html#a32bb087bc325b2ed4c76266687a60a04',1,'SBGAT_GUI::RadarVisualizer::button_box()'],['../class_s_b_g_a_t___g_u_i_1_1_s_h_a_r_m_window.html#add065b2153892dd5aa3902608334e621',1,'SBGAT_GUI::SHARMWindow::button_box()'],['../class_s_b_g_a_t___g_u_i_1_1_surface_p_g_m_window.html#ab6207819c121605320b3d69ed2cae879',1,'SBGAT_GUI::SurfacePGMWindow::button_box()'],['../class_s_b_g_a_t___g_u_i_1_1_y_o_r_p_window.html#aae01547b5cff1fa3094750fef2a40635',1,'SBGAT_GUI::YORPWindow::button_box()']]]
+];
