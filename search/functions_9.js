dd023cefa86b3ce135af42491c35025f154fa84a
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-12-12 20:48:23 -0700

    purge before updates

--- search/functions_9.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['lcvisualizer',['LCVisualizer',['../class_s_b_g_a_t___g_u_i_1_1_l_c_visualizer.html#a20cdf8edaf218c15e8a97b5b37797762',1,'SBGAT_GUI::LCVisualizer']]],
-  ['lcwindow',['LCWindow',['../class_s_b_g_a_t___g_u_i_1_1_l_c_window.html#a7e8a51f5022176fc34b44cca01d34313',1,'SBGAT_GUI::LCWindow']]],
-  ['load_5fattitude_5fstate',['load_attitude_state',['../class_s_b_g_a_t___g_u_i_1_1_shape_properties_widget.html#a44a99867c64810321d788bb3feef325f',1,'SBGAT_GUI::ShapePropertiesWidget']]],
-  ['load_5fposition_5fstate',['load_position_state',['../class_s_b_g_a_t___g_u_i_1_1_shape_properties_widget.html#ab4c37b5529100440383496bf4932b8d1',1,'SBGAT_GUI::ShapePropertiesWidget']]],
-  ['load_5fstate_5ffrom_5ffile',['load_state_from_file',['../class_s_b_g_a_t___g_u_i_1_1_obs_window.html#a6218b08cb5f180d56db0055f142972c4',1,'SBGAT_GUI::ObsWindow']]],
-  ['loadfromjson',['LoadFromJson',['../class_s_b_g_a_t_spherical_harmo.html#a5642cca0e5ce79b83e8de8fdd085a6c3',1,'SBGATSphericalHarmo']]]
-];
