ae799d50f6060302009139e045e79e36d35dbdf9
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-01-29 16:35:13 -0700

    updates to documentation

---
+++ search/files_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['cellpickinteractorstyle_2ehpp',['CellPickInteractorStyle.hpp',['../_cell_pick_interactor_style_8hpp.html',1,'']]],
+  ['constants_2ehpp',['Constants.hpp',['../_constants_8hpp.html',1,'']]]
+];
---
+++ search/functions_3.js
@@ -0,0 +1,24 @@
+var searchData=
+[
+  ['cellpickinteractorstyle',['CellPickInteractorStyle',['../class_s_b_g_a_t___g_u_i_1_1_cell_pick_interactor_style.html#a945f18b0dd644f7076c9768dcfff103c',1,'SBGAT_GUI::CellPickInteractorStyle']]],
+  ['change_5ffocus',['change_focus',['../class_s_b_g_a_t___g_u_i_1_1_rendering_properties_window.html#aca9d1c7da87a24759924aba10618eed8',1,'SBGAT_GUI::RenderingPropertiesWindow']]],
+  ['changed_5fsecondary_5fbox',['changed_secondary_box',['../class_s_b_g_a_t___g_u_i_1_1_obs_window.html#ac283d890d3c5147e89653e14bd23d184',1,'SBGAT_GUI::ObsWindow']]],
+  ['changed_5fselected_5fshape',['changed_selected_shape',['../class_s_b_g_a_t___g_u_i_1_1_s_h_a_r_m_window.html#a470c5a52098ce88166537d0bc100c5b2',1,'SBGAT_GUI::SHARMWindow']]],
+  ['check_5fline_5ffor_5fintersect',['check_line_for_intersect',['../class_s_b_g_a_t_obs.html#a121dd3dc5889c0c3c7da5cf8028b109a',1,'SBGATObs']]],
+  ['checkclosed',['CheckClosed',['../class_s_b_g_a_t_mass_properties.html#a3e5a6460b01be18e38e4b1d0dc7e24f3',1,'SBGATMassProperties']]],
+  ['clear',['Clear',['../class_s_b_g_a_t_polyhedron_gravity_model.html#ae7ad03a3ccbcbb0ca880c5e6d07dde94',1,'SBGATPolyhedronGravityModel::Clear()'],['../class_s_b_g_a_t___g_u_i_1_1_cell_pick_interactor_style.html#aeb78286d74318a559a19a0410b20895d',1,'SBGAT_GUI::CellPickInteractorStyle::Clear()']]],
+  ['clear_5fconsole',['clear_console',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#af58c424287799346a6b3d06e61aaad17',1,'SBGAT_GUI::Mainwindow']]],
+  ['clearimages',['ClearImages',['../class_s_b_g_a_t_obs_radar.html#a3fc51e675cd1e6084e8c2fddd70d91ff',1,'SBGATObsRadar']]],
+  ['collect_5fobservations',['collect_observations',['../class_s_b_g_a_t___g_u_i_1_1_l_c_window.html#a4bc15a7f0bb27f88fcbc782603c77981',1,'SBGAT_GUI::LCWindow::collect_observations()'],['../class_s_b_g_a_t___g_u_i_1_1_radar_window.html#aecd672e5fe2e1cda2148ecf3ad2ec3ed',1,'SBGAT_GUI::RadarWindow::collect_observations()']]],
+  ['collectmeasurements',['CollectMeasurements',['../class_s_b_g_a_t_obs_lightcurve.html#afe5ce4fc5417766406cbf3dba11cad39',1,'SBGATObsLightcurve::CollectMeasurements()'],['../class_s_b_g_a_t_obs_radar.html#ab71d0eeb86803d6a6424e2c83f78188c',1,'SBGATObsRadar::CollectMeasurements()']]],
+  ['compute_5fgeometric_5fmeasures',['compute_geometric_measures',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a515e996b051b87ccf559a376343c07e4',1,'SBGAT_GUI::Mainwindow']]],
+  ['compute_5fsurface_5fpgm',['compute_surface_pgm',['../class_s_b_g_a_t___g_u_i_1_1_surface_p_g_m_window.html#a4f11659c23c8dd108db28affd65eb75a',1,'SBGAT_GUI::SurfacePGMWindow']]],
+  ['computeandsavemassproperties',['ComputeAndSaveMassProperties',['../class_s_b_g_a_t_mass_properties.html#a24fbfd1844ac248648f9a27a70d51f83',1,'SBGATMassProperties']]],
+  ['computesurfacepgm',['ComputeSurfacePGM',['../class_s_b_g_a_t_polyhedron_gravity_model.html#ab061051c7caf81030d9b43931e3cc319',1,'SBGATPolyhedronGravityModel']]],
+  ['contains',['Contains',['../class_s_b_g_a_t_polyhedron_gravity_model.html#a9cf3277f80ae35908db92aa472a82243',1,'SBGATPolyhedronGravityModel']]],
+  ['convert',['convert',['../class_s_b_g_a_t_frame_graph.html#a8593fe37138995b0738bed45b9aee821',1,'SBGATFrameGraph']]],
+  ['convert_5fto_5fchild_5fof_5fprovided_5fparent_5fframe',['convert_to_child_of_provided_parent_frame',['../class_s_b_g_a_t_frame_graph.html#afe959ae72e2270d43626388357694821',1,'SBGATFrameGraph']]],
+  ['convert_5fto_5fparent_5fof_5fprovided_5fchild_5fframe',['convert_to_parent_of_provided_child_frame',['../class_s_b_g_a_t_frame_graph.html#af0380c9cc14ce15c4edf6fba3ee1b867',1,'SBGATFrameGraph']]],
+  ['createactions',['createActions',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a8dfb9599d11c9e91930e4d0343eeb270',1,'SBGAT_GUI::Mainwindow']]],
+  ['createmenus',['createMenus',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#ab59656883d1f3e45d9918aa730d8ee3b',1,'SBGAT_GUI::Mainwindow']]]
+];
