33bfb060097f85d81593a6f462d9495d48213246
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-01-29 16:27:25 -0700

    purge before updates

--- search/classes_3.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['mainwindow',['Mainwindow',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html',1,'SBGAT_GUI']]],
-  ['modeldatawrapper',['ModelDataWrapper',['../class_s_b_g_a_t___g_u_i_1_1_model_data_wrapper.html',1,'SBGAT_GUI']]]
-];
