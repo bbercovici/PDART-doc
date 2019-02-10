771f916f2c7788d219260baefcabb3f737898c6f
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-02-09 22:54:29 -0700

    updates to documentation

---
+++ search/all_14.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['update_5factions_5favailability',['update_actions_availability',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#ab238a1ca2fdb88b7ac180f071d99a233',1,'SBGAT_GUI::Mainwindow']]],
+  ['update_5fdirection',['update_direction',['../class_s_b_g_a_t___g_u_i_1_1_vertex_edition_window.html#a7983b92f28834bdec59696193e7f6ad2',1,'SBGAT_GUI::VertexEditionWindow']]],
+  ['update_5fgui_5fchanged_5fprop',['update_GUI_changed_prop',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#acfb08572289e7df03aadd5e67018f226',1,'SBGAT_GUI::Mainwindow']]],
+  ['update_5fneighborhood_5fsize',['update_neighborhood_size',['../class_s_b_g_a_t___g_u_i_1_1_vertex_edition_window.html#a69d5dd0caf99e666ab97048074eb806e',1,'SBGAT_GUI::VertexEditionWindow']]],
+  ['update_5fneighbors_5fpositions',['update_neighbors_positions',['../class_s_b_g_a_t___g_u_i_1_1_vertex_edition_window.html#af80a182de4ad339aabada4f5f2262902',1,'SBGAT_GUI::VertexEditionWindow']]],
+  ['update_5fphase_5fangle',['update_phase_angle',['../class_s_b_g_a_t___g_u_i_1_1_l_c_window.html#a06c74819b86b133ff8d3457a0b11ba57',1,'SBGAT_GUI::LCWindow']]],
+  ['use_5fgradient_5fcheckbox',['use_gradient_checkbox',['../class_s_b_g_a_t___g_u_i_1_1_settings_window.html#a9555557fb8785c2f916f67d2a7d0609e',1,'SBGAT_GUI::SettingsWindow']]],
+  ['use_5fomp_5fdynamic_5fanalysis',['USE_OMP_DYNAMIC_ANALYSIS',['../_o_m_p__flags_8hpp.html#a44053aa45ca18739271d96180cd10a3a',1,'OMP_flags.hpp']]],
+  ['use_5fomp_5fshape_5fmodel',['USE_OMP_SHAPE_MODEL',['../_o_m_p__flags_8hpp.html#a68f7e60616abbd8b059c7e72e640505d',1,'OMP_flags.hpp']]]
+];
---
+++ search/functions_10.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['toggle_5fattitude_5fvisibility',['toggle_attitude_visibility',['../class_s_b_g_a_t___g_u_i_1_1_shape_properties_widget.html#ace36c2f9303aa18559b555127d9565fe',1,'SBGAT_GUI::ShapePropertiesWidget']]],
+  ['toggle_5fposition_5fvisibility',['toggle_position_visibility',['../class_s_b_g_a_t___g_u_i_1_1_shape_properties_widget.html#aeb7b825fbf5bec179700a913fc2c24cd',1,'SBGAT_GUI::ShapePropertiesWidget']]],
+  ['toggle_5fprop_5fvisibility',['toggle_prop_visibility',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a948a47a8c0ffa35fabc3498237583a2d',1,'SBGAT_GUI::Mainwindow']]],
+  ['translate',['Translate',['../class_s_b_g_a_t_transform_shape.html#a8391f7d3dddf621f31913d2eaaebff77',1,'SBGATTransformShape']]]
+];
