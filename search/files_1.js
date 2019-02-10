a49220006d0616781af30499be50339b3ffe4da8
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-02-09 22:54:15 -0700

    purge before updates

--- search/files_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['cellpickinteractorstyle_2ehpp',['CellPickInteractorStyle.hpp',['../_cell_pick_interactor_style_8hpp.html',1,'']]],
-  ['constants_2ehpp',['Constants.hpp',['../_constants_8hpp.html',1,'']]]
-];
