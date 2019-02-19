57c7544c0a9102c4e0ff7f98edff965fbd7a7784
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-02-19 16:32:57 -0700

    purge before updates

--- search/all_16.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['wrapped_5fshape_5fdata',['wrapped_shape_data',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a9e457fdb34bf2a43f511714b55fbc697',1,'SBGAT_GUI::Mainwindow']]]
-];
