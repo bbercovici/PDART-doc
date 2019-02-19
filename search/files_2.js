6fe6693a74e23c930c94988c574e4bcdd17b747c
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-02-19 16:33:10 -0700

    updates to documentation

---
+++ search/files_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['inheritedpicker_2ehpp',['InheritedPicker.hpp',['../_inherited_picker_8hpp.html',1,'']]]
+];
---
+++ search/functions_5.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['edgeexist',['edgeexist',['../class_adjacency___list.html#a4940d2076901a50659ca83894b945218',1,'Adjacency_List']]],
+  ['edit_5fselection',['edit_selection',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a4fb778758805e0023d9c36ec4054684e',1,'SBGAT_GUI::Mainwindow']]]
+];
