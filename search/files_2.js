1e95f0d60f8ef597e5056d908dded43d703e1b63
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-02-21 10:50:23 -0700

    purge before updates

--- search/files_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['inheritedpicker_2ehpp',['InheritedPicker.hpp',['../_inherited_picker_8hpp.html',1,'']]]
-];
--- search/files_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['pickinteractorstyle_2ehpp',['PickInteractorStyle.hpp',['../_pick_interactor_style_8hpp.html',1,'']]]
-];
