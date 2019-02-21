8479c426361954cbc4360cdad2c9516fb96f13f1
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-02-21 10:53:54 -0700

    updates to documentation

---
+++ search/classes_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['inheritedpicker',['InheritedPicker',['../class_inherited_picker.html',1,'']]]
+];
---
+++ search/files_9.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['vertexeditionwindow_2ehpp',['VertexEditionWindow.hpp',['../_vertex_edition_window_8hpp.html',1,'']]]
+];
