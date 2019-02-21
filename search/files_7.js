b9af24d2f7360f491cd607546f5dd2db41a02d6c
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-02-21 11:04:04 -0700

    purge before updates

--- search/files_7.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['vertexeditionwindow_2ecpp',['VertexEditionWindow.cpp',['../_vertex_edition_window_8cpp.html',1,'']]],
-  ['vertexeditionwindow_2ehpp',['VertexEditionWindow.hpp',['../_vertex_edition_window_8hpp.html',1,'']]]
-];
