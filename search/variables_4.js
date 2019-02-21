a814fff9d4494b7dc879da142b780b20f10913d1
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-02-21 10:41:25 -0700

    purge before updates

--- search/variables_4.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['ecc_5fsbox',['ecc_sbox',['../class_s_b_g_a_t___g_u_i_1_1_shape_properties_widget.html#aab9f9f60dcf8407770447ccc8f1fa6f3',1,'SBGAT_GUI::ShapePropertiesWidget']]],
-  ['edge_5fdyads',['edge_dyads',['../class_s_b_g_a_t_polyhedron_gravity_model.html#ad2b7a8e7b6575d685a775c4f5cc43641',1,'SBGATPolyhedronGravityModel']]],
-  ['edges',['edges',['../class_s_b_g_a_t_polyhedron_gravity_model.html#a00c9fb8bf682d44ca70aebdb4997de79',1,'SBGATPolyhedronGravityModel']]],
-  ['edit_5fselection_5fbutton',['edit_selection_button',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a3f75e4ed1db0dc97cc3d2186ea4be071',1,'SBGAT_GUI::Mainwindow']]]
-];
