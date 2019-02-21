0272e51079905c11db45181059c51c409e07f9a8
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-02-21 10:46:19 -0700

    purge before updates

--- search/variables_11.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['totalmass',['totalMass',['../class_s_b_g_a_t_spherical_harmo.html#a90cd173047adc19a6e2018f04eb83be1',1,'SBGATSphericalHarmo']]],
-  ['tree',['tree',['../class_s_b_g_a_t___g_u_i_1_1_model_data_wrapper.html#af6929c8ab1ee5f8e330e86d2183698b9',1,'SBGAT_GUI::ModelDataWrapper']]]
-];
