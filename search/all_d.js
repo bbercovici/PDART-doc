836f1f371ad57b5b9ccc169c406d9c0b05dd6ab6
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-02-21 10:41:41 -0700

    updates to documentation

---
+++ search/all_d.js
@@ -0,0 +1,24 @@
+var searchData=
+[
+  ['n_5fedges',['N_edges',['../class_s_b_g_a_t_polyhedron_gravity_model.html#acf22beed8cbf32180c81bead92c22ebb',1,'SBGATPolyhedronGravityModel']]],
+  ['n_5ffacets',['n_facets',['../class_s_b_g_a_t_spherical_harmo.html#a39408276805d16b0699d427552428387',1,'SBGATSphericalHarmo::n_facets()'],['../class_s_b_g_a_t_polyhedron_gravity_model.html#a90aab7c4db26bddb3e2b7e01c52da1a6',1,'SBGATPolyhedronGravityModel::N_facets()']]],
+  ['n_5fimages_5fsbox',['N_images_sbox',['../class_s_b_g_a_t___g_u_i_1_1_obs_window.html#a7052d15f3065ce15cffb280fdfa01080',1,'SBGAT_GUI::ObsWindow']]],
+  ['n_5fsamples_5fsbox',['N_samples_sbox',['../class_s_b_g_a_t___g_u_i_1_1_obs_window.html#ae7cbb9a5ed0fcf146c9576a82fedff92',1,'SBGAT_GUI::ObsWindow']]],
+  ['n_5fvertices',['n_vertices',['../class_s_b_g_a_t_spherical_harmo.html#acc94045ae3f564c93569f3aff50f27c7',1,'SBGATSphericalHarmo']]],
+  ['name',['name',['../class_s_b_g_a_t_ref_frame.html#a7e3971838d1797a5903878d3e72f6cb2',1,'SBGATRefFrame']]],
+  ['neighborhood_5fspin_5fbox',['neighborhood_spin_box',['../class_s_b_g_a_t___g_u_i_1_1_vertex_edition_window.html#a28d8460c96c2c86e18414d73f9b716eb',1,'SBGAT_GUI::VertexEditionWindow']]],
+  ['neighbors_5factors_5fvector',['neighbors_actors_vector',['../class_s_b_g_a_t___g_u_i_1_1_vertex_edition_window.html#a9bebe11ba22fab77cbab71b5e3bf847f',1,'SBGAT_GUI::VertexEditionWindow']]],
+  ['neighbors_5fdistances_5fvector',['neighbors_distances_vector',['../class_s_b_g_a_t___g_u_i_1_1_vertex_edition_window.html#aada44b29d571cd74821a7ea43c8dde96',1,'SBGAT_GUI::VertexEditionWindow']]],
+  ['neighbors_5findices_5fvector',['neighbors_indices_vector',['../class_s_b_g_a_t___g_u_i_1_1_vertex_edition_window.html#a773a66641f638910a7323c7a08c266fe',1,'SBGAT_GUI::VertexEditionWindow']]],
+  ['new',['New',['../class_s_b_g_a_t_mass_properties.html#a38b247e0f58c4d1baab1da788ba9ba59',1,'SBGATMassProperties::New()'],['../class_s_b_g_a_t_obj_writer.html#afee3a69fc653616496bf37acc402c281',1,'SBGATObjWriter::New()'],['../class_s_b_g_a_t_obs.html#a3d9177b469b75bb062c578fea594b077',1,'SBGATObs::New()'],['../class_s_b_g_a_t_obs_lightcurve.html#ab0e2a9eac3d25cad011231c7e65c0546',1,'SBGATObsLightcurve::New()'],['../class_s_b_g_a_t_obs_radar.html#a284d19c34bf5963f068326e80112fe37',1,'SBGATObsRadar::New()'],['../class_s_b_g_a_t_polyhedron_gravity_model.html#ad5f89271ab3eab654dc16dc9341b0d55',1,'SBGATPolyhedronGravityModel::New()'],['../class_s_b_g_a_t_shape_uncertainty.html#ad0cddfdbc60179e6417793f3629e2c66',1,'SBGATShapeUncertainty::New()'],['../class_s_b_g_a_t_spherical_harmo.html#aede73742caf7f156597bb1bbf7c0cdaa',1,'SBGATSphericalHarmo::New()'],['../class_s_b_g_a_t_srp_yorp.html#af8f7f506dad3ccbe09131a7476798a59',1,'SBGATSrpYorp::New()'],['../class_s_b_g_a_t___g_u_i_1_1_pick_interactor_style.html#a0aa462c2a157fca709ffdd285fca997a',1,'SBGAT_GUI::PickInteractorStyle::New()']]],
+  ['next_5fimage',['next_image',['../class_s_b_g_a_t___g_u_i_1_1_radar_visualizer.html#a478a97c6698045718c667d70a0c39d59',1,'SBGAT_GUI::RadarVisualizer']]],
+  ['next_5fimage_5fbutton',['next_image_button',['../class_s_b_g_a_t___g_u_i_1_1_radar_visualizer.html#a6c0b5706916fe6363e21c2cbb31bfe07',1,'SBGAT_GUI::RadarVisualizer']]],
+  ['non_5fnormalized_5fbutton',['non_normalized_button',['../class_s_b_g_a_t___g_u_i_1_1_s_h_a_r_m_window.html#ab30ad425aa22c6034882acbf956531a4',1,'SBGAT_GUI::SHARMWindow']]],
+  ['normalized',['normalized',['../class_s_b_g_a_t_spherical_harmo.html#ace4b773066a489f4b7331e01dbfd6924',1,'SBGATSphericalHarmo']]],
+  ['normalized_5fbutton',['normalized_button',['../class_s_b_g_a_t___g_u_i_1_1_s_h_a_r_m_window.html#af73709ecfc8bb3d885fc1a4c35a3ed10',1,'SBGAT_GUI::SHARMWindow']]],
+  ['normalized_5fprincipal_5fmoments',['normalized_principal_moments',['../class_s_b_g_a_t_mass_properties.html#af7d1a619ca006ef404212535a468f8ca',1,'SBGATMassProperties']]],
+  ['normalizedshapeindex',['NormalizedShapeIndex',['../class_s_b_g_a_t_mass_properties.html#a6d6f2fe3c52bb9d61018d4bbb71ebeb4',1,'SBGATMassProperties']]],
+  ['number_5fof_5fbodies',['number_of_bodies',['../class_s_b_g_a_t_obs.html#a8b738607c3bc5b3c29b6ae49f94b2019',1,'SBGATObs']]],
+  ['numrefine',['numrefine',['../class_s_b_g_a_t_srp_yorp.html#ab137290c375e8b778fde5fbb3bcb82a6',1,'SBGATSrpYorp']]],
+  ['numvox',['numVox',['../class_s_b_g_a_t_srp_yorp.html#ae68be75a08df37e79ae23f0b36518ad2',1,'SBGATSrpYorp']]]
+];
---
+++ search/files_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['pickinteractorstyle_2ehpp',['PickInteractorStyle.hpp',['../_pick_interactor_style_8hpp.html',1,'']]]
+];
