e2469a4cee6aeb3822b8b5735bd6b9564b062d39
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-02-21 11:00:45 -0700

    purge before updates

--- search/functions_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['bin_5fobservations',['bin_observations',['../class_s_b_g_a_t___g_u_i_1_1_radar_window.html#a06d119d2f6bc7abfce459ac8bc136320',1,'SBGAT_GUI::RadarWindow']]],
-  ['binobservations',['BinObservations',['../class_s_b_g_a_t_obs_radar.html#a459b4a782aa1907d19473c8a457c34e4',1,'SBGATObsRadar']]]
-];
