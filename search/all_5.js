6374eac1883d64a6af749600427b7ea27ddf8511
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-04-08 20:41:45 -0600

    purge before updates

--- search/all_5.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['facet_5fselection_5fmode',['facet_selection_mode',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#acda54e3be83fdad6c10b67b57e98dd9a',1,'SBGAT_GUI::Mainwindow']]],
-  ['fourier_5fcombo_5fbox',['fourier_combo_box',['../class_s_b_g_a_t___g_u_i_1_1_y_o_r_p_window.html#ab476ccf8eeb22801b6c79790331bddad',1,'SBGAT_GUI::YORPWindow']]],
-  ['frame_5fgraph',['frame_graph',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a8f4575113e19be7dcd4ca43d9c05b77b',1,'SBGAT_GUI::Mainwindow']]]
-];
