e6f867be478ab23a59db6a9cd3ee455ac17ee295
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-04-08 20:44:31 -0600

    purge before updates

--- search/all_4.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['ecc_5fsbox',['ecc_sbox',['../class_s_b_g_a_t___g_u_i_1_1_shape_properties_widget.html#aab9f9f60dcf8407770447ccc8f1fa6f3',1,'SBGAT_GUI::ShapePropertiesWidget']]],
-  ['edit_5fselection',['edit_selection',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#aadb2ac204b6c22643eae8ea68dc32f5b',1,'SBGAT_GUI::Mainwindow']]],
-  ['edit_5fselection_5fbutton',['edit_selection_button',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a3f75e4ed1db0dc97cc3d2186ea4be071',1,'SBGAT_GUI::Mainwindow']]]
-];
--- search/functions_10.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['yorpwindow',['YORPWindow',['../class_s_b_g_a_t___g_u_i_1_1_y_o_r_p_window.html#abbb6c02898983f5ab655ca71b315f4a3',1,'SBGAT_GUI::YORPWindow']]]
-];
