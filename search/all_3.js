63764db6a6cec07080c6fae0a12a209dc099cf85
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-04-08 20:42:02 -0600

    updates to documentation

---
+++ search/all_3.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['data',['Data',['../class_s_b_g_a_t___g_u_i_1_1_pick_interactor_style.html#a46dd8a587c910e5825720d356e337d77',1,'SBGAT_GUI::PickInteractorStyle']]],
+  ['datamap',['DataMap',['../namespace_s_b_g_a_t___g_u_i.html#ab241a472b909070057b89916b570e8b2',1,'SBGAT_GUI']]],
+  ['degree_5fcombo_5fbox',['degree_combo_box',['../class_s_b_g_a_t___g_u_i_1_1_s_h_a_r_m_window.html#a044a16d397c8c3f465608da2aae0f608',1,'SBGAT_GUI::SHARMWindow']]],
+  ['delta_5fdel_5fsbox',['delta_del_sbox',['../class_s_b_g_a_t___g_u_i_1_1_y_o_r_p_window.html#a56d8b6b727aa1f848e87fad5b9738b69',1,'SBGAT_GUI::YORPWindow']]],
+  ['density_5fsbox',['density_sbox',['../class_s_b_g_a_t___g_u_i_1_1_shape_properties_widget.html#ae43401659b4ee0f0a358e6af25923f57',1,'SBGAT_GUI::ShapePropertiesWidget::density_sbox()'],['../class_s_b_g_a_t___g_u_i_1_1_s_h_a_r_m_window.html#a281bb04cf5715a32a6b16da6311a00e1',1,'SBGAT_GUI::SHARMWindow::density_sbox()']]],
+  ['dir',['dir',['../class_s_b_g_a_t___g_u_i_1_1_vertex_edition_window.html#aece21576cf81f8a517decf63a8fc8ad0',1,'SBGAT_GUI::VertexEditionWindow']]],
+  ['direction_5fcombo_5fbox',['direction_combo_box',['../class_s_b_g_a_t___g_u_i_1_1_vertex_edition_window.html#aa9703f6a066bd1c42b742cc619543584',1,'SBGAT_GUI::VertexEditionWindow']]]
+];
---
+++ search/variables_9.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['n_5fimages_5fsbox',['N_images_sbox',['../class_s_b_g_a_t___g_u_i_1_1_obs_window.html#a7052d15f3065ce15cffb280fdfa01080',1,'SBGAT_GUI::ObsWindow']]],
+  ['n_5fsamples_5fsbox',['N_samples_sbox',['../class_s_b_g_a_t___g_u_i_1_1_obs_window.html#ae7cbb9a5ed0fcf146c9576a82fedff92',1,'SBGAT_GUI::ObsWindow']]],
+  ['neighborhood_5fspin_5fbox',['neighborhood_spin_box',['../class_s_b_g_a_t___g_u_i_1_1_vertex_edition_window.html#a28d8460c96c2c86e18414d73f9b716eb',1,'SBGAT_GUI::VertexEditionWindow']]],
+  ['neighbors_5factors_5fvector',['neighbors_actors_vector',['../class_s_b_g_a_t___g_u_i_1_1_vertex_edition_window.html#a9bebe11ba22fab77cbab71b5e3bf847f',1,'SBGAT_GUI::VertexEditionWindow']]],
+  ['neighbors_5fdistances_5fvector',['neighbors_distances_vector',['../class_s_b_g_a_t___g_u_i_1_1_vertex_edition_window.html#aada44b29d571cd74821a7ea43c8dde96',1,'SBGAT_GUI::VertexEditionWindow']]],
+  ['neighbors_5findices_5fvector',['neighbors_indices_vector',['../class_s_b_g_a_t___g_u_i_1_1_vertex_edition_window.html#a773a66641f638910a7323c7a08c266fe',1,'SBGAT_GUI::VertexEditionWindow']]],
+  ['next_5fimage_5fbutton',['next_image_button',['../class_s_b_g_a_t___g_u_i_1_1_radar_visualizer.html#a6c0b5706916fe6363e21c2cbb31bfe07',1,'SBGAT_GUI::RadarVisualizer']]],
+  ['non_5fnormalized_5fbutton',['non_normalized_button',['../class_s_b_g_a_t___g_u_i_1_1_s_h_a_r_m_window.html#ab30ad425aa22c6034882acbf956531a4',1,'SBGAT_GUI::SHARMWindow']]],
+  ['normalized_5fbutton',['normalized_button',['../class_s_b_g_a_t___g_u_i_1_1_s_h_a_r_m_window.html#af73709ecfc8bb3d885fc1a4c35a3ed10',1,'SBGAT_GUI::SHARMWindow']]]
+];
