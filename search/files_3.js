a08ff6f66d7cf87fa35d97c407985b3bca7dbad0
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-04-08 20:37:18 -0600

    purge before updates

--- search/files_3.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['obswindow_2ecpp',['ObsWindow.cpp',['../_obs_window_8cpp.html',1,'']]],
-  ['obswindow_2ehpp',['ObsWindow.hpp',['../_obs_window_8hpp.html',1,'']]]
-];
--- search/files_8.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['yorpwindow_2ecpp',['YORPWindow.cpp',['../_y_o_r_p_window_8cpp.html',1,'']]],
-  ['yorpwindow_2ehpp',['YORPWindow.hpp',['../_y_o_r_p_window_8hpp.html',1,'']]]
-];
