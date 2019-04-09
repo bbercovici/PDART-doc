37b818b1ac05a988b8366b3dcf89382cbadd3ed4
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-04-08 20:38:08 -0600

    purge before updates

--- search/functions_6.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['lcvisualizer',['LCVisualizer',['../class_s_b_g_a_t___g_u_i_1_1_l_c_visualizer.html#a54c8ba855c14ba992bcef1b3ff902ac1',1,'SBGAT_GUI::LCVisualizer']]],
-  ['lcwindow',['LCWindow',['../class_s_b_g_a_t___g_u_i_1_1_l_c_window.html#aa2a67cf8dae1289b524d553efecdbb09',1,'SBGAT_GUI::LCWindow']]],
-  ['load_5fattitude_5fstate',['load_attitude_state',['../class_s_b_g_a_t___g_u_i_1_1_shape_properties_widget.html#a419fc33812247d964cddd8de39bfc6bc',1,'SBGAT_GUI::ShapePropertiesWidget']]],
-  ['load_5fposition_5fstate',['load_position_state',['../class_s_b_g_a_t___g_u_i_1_1_shape_properties_widget.html#aa663d108d1c4fa7dad8c8e482ad98070',1,'SBGAT_GUI::ShapePropertiesWidget']]],
-  ['load_5fstate_5ffrom_5ffile',['load_state_from_file',['../class_s_b_g_a_t___g_u_i_1_1_obs_window.html#af8ac65d1244d63e9f2b6c1f16f5725d9',1,'SBGAT_GUI::ObsWindow']]],
-  ['load_5fsurface_5fpgm',['load_surface_pgm',['../class_s_b_g_a_t___g_u_i_1_1_surface_p_g_m_window.html#a3bd4cd75a912669779ed3f73564a5975',1,'SBGAT_GUI::SurfacePGMWindow']]]
-];
--- search/functions_a.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['pickinteractorstyle',['PickInteractorStyle',['../class_s_b_g_a_t___g_u_i_1_1_pick_interactor_style.html#a8a3212c8b1ca2f8d203f01f1854570b5',1,'SBGAT_GUI::PickInteractorStyle']]],
-  ['previous_5fimage',['previous_image',['../class_s_b_g_a_t___g_u_i_1_1_radar_visualizer.html#a722432be8a6af49091e47167a1914ed2',1,'SBGAT_GUI::RadarVisualizer']]],
-  ['prop_5fadded_5fsignal',['prop_added_signal',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a52f69fb4ec81f7d3f5ad40b29d3a4185',1,'SBGAT_GUI::Mainwindow']]],
-  ['prop_5fadded_5fslot',['prop_added_slot',['../class_s_b_g_a_t___g_u_i_1_1_rendering_properties_window.html#a0519cfb9c8d2356d58226d97b5f20ca3',1,'SBGAT_GUI::RenderingPropertiesWindow']]],
-  ['prop_5fremoved_5fsignal',['prop_removed_signal',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#ab9adac17c4e9c2bb5862fc4b4f98846d',1,'SBGAT_GUI::Mainwindow']]],
-  ['prop_5fremoved_5fslot',['prop_removed_slot',['../class_s_b_g_a_t___g_u_i_1_1_rendering_properties_window.html#ac31fe94a25913b6ce38451b7fbbd6c4e',1,'SBGAT_GUI::RenderingPropertiesWindow']]]
-];
