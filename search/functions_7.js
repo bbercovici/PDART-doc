62ee95217b0ec91b54051c29729fee49f36d7790
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-04-08 20:41:32 -0600

    updates to documentation

---
+++ search/functions_7.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['main',['main',['../main_8cpp.html#a3c04138a5bfe5d72780bb7e82a18e627',1,'main.cpp']]],
+  ['mainwindow',['Mainwindow',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a9ca049b84f17c87a57b53e418b1c40e3',1,'SBGAT_GUI::Mainwindow']]],
+  ['modeldatawrapper',['ModelDataWrapper',['../class_s_b_g_a_t___g_u_i_1_1_model_data_wrapper.html#ad507825d6efcd9a821711fa74fa5789b',1,'SBGAT_GUI::ModelDataWrapper']]],
+  ['move_5fvertex',['move_vertex',['../class_s_b_g_a_t___g_u_i_1_1_vertex_edition_window.html#a65684fdbbec643682d3d184174f8e8a5',1,'SBGAT_GUI::VertexEditionWindow']]]
+];
---
+++ search/variables_11.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['view',['view',['../class_s_b_g_a_t___g_u_i_1_1_l_c_visualizer.html#aa1f9bd4f6568507533ab123218e94854',1,'SBGAT_GUI::LCVisualizer::view()'],['../class_s_b_g_a_t___g_u_i_1_1_radar_visualizer.html#a786a7af3728e95f12081e8a36d359ca2',1,'SBGAT_GUI::RadarVisualizer::view()']]],
+  ['voxel_5fcombo_5fbox',['voxel_combo_box',['../class_s_b_g_a_t___g_u_i_1_1_y_o_r_p_window.html#ab6ab5fc20f0a2f5967d600d8d6bdd389',1,'SBGAT_GUI::YORPWindow']]]
+];
