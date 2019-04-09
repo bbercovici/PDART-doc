008dff72e118a09d66b67faa4e4366ab864d5b7a
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-04-08 20:30:44 -0600

    updates to documentation

---
+++ search/all_e.js
@@ -0,0 +1,25 @@
+var searchData=
+[
+  ['r_5fbin_5fsbox',['r_bin_sbox',['../class_s_b_g_a_t___g_u_i_1_1_radar_window.html#a7c4de3b7a8cc35a21274989cc6d04122',1,'SBGAT_GUI::RadarWindow']]],
+  ['radar_5faz_5fsbox',['radar_az_sbox',['../class_s_b_g_a_t___g_u_i_1_1_radar_window.html#a78cea9b030b1ab11c485f5b025d5b908',1,'SBGAT_GUI::RadarWindow']]],
+  ['radar_5fel_5fsbox',['radar_el_sbox',['../class_s_b_g_a_t___g_u_i_1_1_radar_window.html#a1f3ac3a942f9e601a66b30ad640c68c9',1,'SBGAT_GUI::RadarWindow']]],
+  ['radarvisualizer',['RadarVisualizer',['../class_s_b_g_a_t___g_u_i_1_1_radar_visualizer.html',1,'SBGAT_GUI::RadarVisualizer'],['../class_s_b_g_a_t___g_u_i_1_1_radar_visualizer.html#ae4ae11e8399f7600482b4cc216a20388',1,'SBGAT_GUI::RadarVisualizer::RadarVisualizer()']]],
+  ['radarvisualizer_2ecpp',['RadarVisualizer.cpp',['../_radar_visualizer_8cpp.html',1,'']]],
+  ['radarvisualizer_2ehpp',['RadarVisualizer.hpp',['../_radar_visualizer_8hpp.html',1,'']]],
+  ['radarwindow',['RadarWindow',['../class_s_b_g_a_t___g_u_i_1_1_radar_window.html',1,'SBGAT_GUI::RadarWindow'],['../class_s_b_g_a_t___g_u_i_1_1_radar_window.html#aa4d7b86b3dfa674d000fda56de0cc6b1',1,'SBGAT_GUI::RadarWindow::RadarWindow()']]],
+  ['radarwindow_2ecpp',['RadarWindow.cpp',['../_radar_window_8cpp.html',1,'']]],
+  ['radarwindow_2ehpp',['RadarWindow.hpp',['../_radar_window_8hpp.html',1,'']]],
+  ['readme_2emd',['README.md',['../_r_e_a_d_m_e_8md.html',1,'']]],
+  ['ref_5fradius_5fsbox',['ref_radius_sbox',['../class_s_b_g_a_t___g_u_i_1_1_s_h_a_r_m_window.html#ad7df3f92f6bd11e2af09d3434ae2ef98',1,'SBGAT_GUI::SHARMWindow']]],
+  ['refine_5fcombo_5fbox',['refine_combo_box',['../class_s_b_g_a_t___g_u_i_1_1_y_o_r_p_window.html#aaacad6af8c440aa0d0ede54df34c142a',1,'SBGAT_GUI::YORPWindow']]],
+  ['remove_5fprop',['remove_prop',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#af926008d86e47915910478ca869904fe',1,'SBGAT_GUI::Mainwindow']]],
+  ['renderer',['renderer',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#ab40630e7d9734765032b79aa68f39e37',1,'SBGAT_GUI::Mainwindow']]],
+  ['renderingpropertieswindow',['RenderingPropertiesWindow',['../class_s_b_g_a_t___g_u_i_1_1_rendering_properties_window.html',1,'SBGAT_GUI::RenderingPropertiesWindow'],['../class_s_b_g_a_t___g_u_i_1_1_rendering_properties_window.html#a1f36d5cf5b7aa5eded34f9073b3faa45',1,'SBGAT_GUI::RenderingPropertiesWindow::RenderingPropertiesWindow()']]],
+  ['renderingpropertieswindow_2ecpp',['RenderingPropertiesWindow.cpp',['../_rendering_properties_window_8cpp.html',1,'']]],
+  ['renderingpropertieswindow_2ehpp',['RenderingPropertiesWindow.hpp',['../_rendering_properties_window_8hpp.html',1,'']]],
+  ['resultsmenu',['ResultsMenu',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#ac6d9b2de027e76f7612c1241f96119a7',1,'SBGAT_GUI::Mainwindow']]],
+  ['rgb_5fbutton',['rgb_button',['../class_s_b_g_a_t___g_u_i_1_1_settings_window.html#a62c7a9c6df790af92a443134699c1ac8',1,'SBGAT_GUI::SettingsWindow']]],
+  ['rho_5fsbox',['rho_sbox',['../class_s_b_g_a_t___g_u_i_1_1_y_o_r_p_window.html#a88a787d71319299dc92cfa8b81aef496',1,'SBGAT_GUI::YORPWindow']]],
+  ['right_5fdockwidget',['right_dockwidget',['../class_s_b_g_a_t___g_u_i_1_1_mainwindow.html#a6aa05e882aa48e5ca6bda39cd3f67183',1,'SBGAT_GUI::Mainwindow']]],
+  ['rr_5fbin_5fsbox',['rr_bin_sbox',['../class_s_b_g_a_t___g_u_i_1_1_radar_window.html#a3037ee920a7eb670e15d16269941bafe',1,'SBGAT_GUI::RadarWindow']]]
+];
