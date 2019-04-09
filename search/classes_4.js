89f17cc08a313614073b4e7b5f0ab690312f3322
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-04-08 20:50:41 -0600

    purge before updates

--- search/classes_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['pickinteractorstyle',['PickInteractorStyle',['../class_s_b_g_a_t___g_u_i_1_1_pick_interactor_style.html',1,'SBGAT_GUI']]]
-];
--- search/files_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['inheritedpicker_2ehpp',['InheritedPicker.hpp',['../_inherited_picker_8hpp.html',1,'']]],
-  ['interactorstyle_2ecpp',['InteractorStyle.cpp',['../_interactor_style_8cpp.html',1,'']]]
-];
