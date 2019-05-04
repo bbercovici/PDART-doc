60395a4406ab7c6017a9a3c7e50fce78569499ed
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-05-04 20:19:12 +0200

    purge before updates

--- search/classes_12.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['wide_5fstring_5finput_5fadapter',['wide_string_input_adapter',['../classnlohmann_1_1detail_1_1wide__string__input__adapter.html',1,'nlohmann::detail']]]
-];
--- search/variables_11.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['thousands_5fsep',['thousands_sep',['../classnlohmann_1_1detail_1_1serializer.html#a78a6ae833bb6cf7f00cb0d51db114b14',1,'nlohmann::detail::serializer']]],
-  ['token_5fbuffer',['token_buffer',['../classnlohmann_1_1detail_1_1lexer.html#a8f43746570e5cadbc9b2b6b0c4c8e051',1,'nlohmann::detail::lexer']]],
-  ['token_5fstring',['token_string',['../classnlohmann_1_1detail_1_1lexer.html#ad2960e3d54af8fb8d572a8f6f7731d62',1,'nlohmann::detail::lexer']]],
-  ['totalmass',['totalMass',['../class_s_b_g_a_t_spherical_harmo.html#a90cd173047adc19a6e2018f04eb83be1',1,'SBGATSphericalHarmo']]],
-  ['tree',['tree',['../class_s_b_g_a_t___g_u_i_1_1_model_data_wrapper.html#af6929c8ab1ee5f8e330e86d2183698b9',1,'SBGAT_GUI::ModelDataWrapper']]]
-];
